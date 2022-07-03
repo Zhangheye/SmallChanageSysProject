a7bf44e022f83391f70b64b5ea9b58c3e8a99b30
Zhangheye <2593238552@qq.com>
2022-07-03 21:53:19 +0800

    4 commit

--- src/com/jxut/SmallChanageSys.java
+++ src/com/jxut/SmallChanageSys.java
@@ -2,8 +2,8 @@ package com.jxut;
 
 /**
  * 该类是完成零钱通的各个功能的类
- *  各功能对应一个方法
- * */
+ * 各功能对应一个方法
+ */
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -27,51 +27,57 @@ public class SmallChanageSys {
     ArrayList<String> strList = new ArrayList<String>();
 
     //零钱通菜单
-    public  void menu(){
-        while (true){
+    public void menu() {
+        while (true) {
             System.out.println("--------------零钱通菜单--------------");
             System.out.println("\t\t\t\t1 零钱通明细");
             System.out.println("\t\t\t\t2 收益入账");
             System.out.println("\t\t\t\t3 消费账单");
             System.out.println("\t\t\t\t4 退  出");
             System.out.println("请选择(1-4):");
+            //用户进行选择
             select();
-
-            if (quit){
+            if (quit) {
                 break;
             }
         }
     }
 
-    private  void select() {
+    //选择功能
+    private void select() {
         Scanner s = new Scanner(System.in);
         int anInt = s.nextInt();
-        if (anInt == 1){
+        if (anInt == 1) {
+            //零钱通明细
             detail();
-        }else if (anInt == 2){
+        } else if (anInt == 2) {
+            //收益入账
             earnings();
-        }else if (anInt==3){
+        } else if (anInt == 3) {
+            //消费账单
             consumption();
-        }else if (anInt == 4){
+        } else if (anInt == 4) {
+            //退出
             quit();
         }
     }
 
+    //退出方法
     private boolean quit() {
         System.out.println("请确认是否退出(y/n):");
         Scanner s = new Scanner(System.in);
         String s1 = s.nextLine();
-        if (s1.equals("y")){
+        if (s1.equals("y")) {
             return quit = true;
-        }else
+        } else
             System.out.println("取消退出!");
 
         return false;
     }
 
     //消费账单
-    private  void consumption() {
-        if (balance <= 0){
+    private void consumption() {
+        if (balance <= 0) {
             System.out.println("对不起，您的余额为零！无法消费！");
         }
 
@@ -79,7 +85,7 @@ public class SmallChanageSys {
         Scanner ss = new Scanner(System.in);
         int anInt = ss.nextInt();
 
-        if (anInt > balance){
+        if (anInt > balance) {
             System.out.println("对不起，您的消费金额大于余额！消费失败！");
         }
 
@@ -87,30 +93,33 @@ public class SmallChanageSys {
         Scanner s = new Scanner(System.in);
         String s1 = s.nextLine();
 
-
+        //修改余额
         balance -= anInt;
 
-        strList.add(s1 +"\t"+ "-"+anInt +"\t"+ format+" 余额:" +balance);
+        //把消费详情添加到集合中
+        strList.add(s1 + "\t" + "-" + anInt + "\t" + format + " 余额:" + balance);
     }
 
     //收益入账
-    private  void earnings() {
+    private void earnings() {
 
         System.out.println("请输入你的收益：");
         Scanner s = new Scanner(System.in);
         int anInt = s.nextInt();
-        if (anInt <= 0){
+        if (anInt <= 0) {
             System.out.println("入账无法小于或等于0！请重新输入！");
         }
-        balance +=anInt;
+        //修改余额
+        balance += anInt;
 
-        strList.add("收益入账\t+" + anInt +"\t"+format+" 余额:" +balance );
+        //把收起详情添加到集合中
+        strList.add("收益入账\t+" + anInt + "\t" + format + " 余额:" + balance);
     }
 
     //零钱通明细
-    private  void detail() {
+    private void detail() {
         System.out.println("--------------零钱通明细--------------");
-        for (String s :strList){
+        for (String s : strList) {
             System.out.println(s);
         }
     }
--- src/com/jxut/Test.java
+++ src/com/jxut/Test.java
@@ -1,7 +1,10 @@
 package com.jxut;
-
+/**
+ * 测试零钱通
+ * */
 public class Test {
     public static void main(String[] args) {
+        //调用零钱通类的菜单方法
         new SmallChanageSys().menu();
     }
 }
